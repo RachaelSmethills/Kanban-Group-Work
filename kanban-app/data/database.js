8afaf71839bcb1d441257d3497c97c85a2b11677
Rachael Ann Smethills <ras140899@aol.com>
2020-11-04 14:52:16 +0000

    ongoing

--- kanban-app/data/database.js
+++ kanban-app/data/database.js
@@ -1,8 +1,20 @@
 const { Sequelize, Model, DataTypes } = require('sequelize'),
-    sequelize = new Sequelize('sqlite::memory:', {
-        logging: false
-    }),
     defaultData = require('../defaultdata.json');
+const path = require('path');
+require('dotenv').config();
+
+const connectionSettings = {
+    test: { dialect: 'sqlite', storage: 'sqlite::memory:' },
+    dev: { dialect: 'sqlite', storage: path.join(__dirname, 'data.db'), logging: false },
+    production: { dialect: 'postgres', protocal: 'postgres' }
+}
+
+const moo = process.env.NODE_ENV === 'production';
+console.log('we are runnig in production', moo, process.env.NODE_ENV);
+
+const sequelize = process.env.NODE_ENV === 'production' ?
+    new Sequelize(process.env.DATABASE_URL, connectionSettings[process.env.NODE_ENV]) :
+    new Sequelize(connectionSettings[process.env.NODE_ENV]);
 
 class User extends Model {}
 class Project extends Model {}
